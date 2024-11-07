5229a27b63e8f30820ef53e417b8db96b73677f6
Yuliia Voziian <vozianulia@gmail.com>
2024-11-07 22:22:39 +0200

    add read and write function

--- src/utils/writeContacts.js
+++ src/utils/writeContacts.js
@@ -1,3 +1,13 @@
 import { PATH_DB } from '../constants/contacts.js';
+import fs from 'node:fs/promises';
 
-export const writeContacts = async (updatedContacts) => {};
+export const writeContacts = async (updatedContacts) => {
+  try {
+    await fs.writeFile(PATH_DB, JSON.stringify(updatedContacts, undefined, 2), {
+      encoding: 'utf-8',
+    });
+    console.log('Дані успішно записані у файл');
+  } catch (error) {
+    console.error('Помилка запису у файл:', error);
+  }
+};
