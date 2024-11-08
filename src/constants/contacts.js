b90a30f0ed8090e483b014fd970b79b79e45c6ee
Yuliia Voziian <vozianulia@gmail.com>
2024-11-08 13:19:30 +0200

    update two files

--- src/constants/contacts.js
+++ src/constants/contacts.js
@@ -1,1 +1,7 @@
-export const PATH_DB = require('../db/db.json');
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url); // Получаем абсолютный путь к файлу
+const __dirname = path.dirname(__filename); // Получаем абсолютный путь к директории
+
+export const PATH_DB = path.join(__dirname, '../db/db.json');
--- src/scripts/addOneContact.js
+++ src/scripts/addOneContact.js
@@ -1,3 +1,19 @@
-export const addOneContact = async () => {};
+import { createFakeContact } from '../utils/createFakeContact.js';
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+
+export const addOneContact = async () => {
+  try {
+    const existingContacts = (await readContacts()) || [];
+
+    const newContact = createFakeContact();
+    const updatedContacts = [...existingContacts, newContact];
+
+    await writeContacts(updatedContacts);
+    console.log('Новый контакт успешно добавлен');
+  } catch (error) {
+    console.error('Ошибка при добавлении контакта:', error);
+  }
+};
 
 addOneContact();
